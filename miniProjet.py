a1c68db8de79466128856587ffbcf3265f28e4bf
MacBook <macbook@associationkasrelbadia@gmail.com>
2020-05-04 22:16:14 +0000

    ajout des fcts

--- miniProjet.py
+++ miniProjet.py
@@ -1,10 +1,12 @@
 import sqlite3 as sql
+import matplotlib
+matplotlib.use('TkAgg')
 conn=sql.connect("/Users/macbook/Documents/S6/miniProjet/student.db")
 cur=conn.cursor()
 
 # creation des tables
 
-cur.execute(" CREATE TABLE IF NOT EXISTS Auteur ( Nauteur Integer,nomA text,prenomA text,nationaliteA text,PRIMARY KEY(Nauteur) ) ")
+"""cur.execute(" CREATE TABLE IF NOT EXISTS Auteur ( Nauteur Integer,nomA text,prenomA text,nationaliteA text,PRIMARY KEY(Nauteur) ) ")
 cur.execute(" CREATE TABLE IF NOT EXISTS Livre ( Nlivre Integer,num_ISBN Integer,titre text,nbPages Integer,anneeS Integer,prix real, PRIMARY KEY(Nlivre) ) ")
 cur.execute(" CREATE TABLE IF NOT EXISTS Possede (Nlivre Integer, Nauteur Integer, FOREIGN KEY(Nlivre) REFERENCES Livre(Nlivre), FOREIGN KEY(Nauteur) REFERENCES Auteur(Nauteur) ) ")
 cur.execute(" CREATE TABLE IF NOT EXISTS Pret ( Npret Integer,num_etu Integer,Nlivre Integer,datePret text,dateRetour text,DateRetourPrevue text, PRIMARY KEY(Npret), FOREIGN KEY(num_etu) REFERENCES Etudiant(num_etu), FOREIGN KEY(Nlivre) REFERENCES Livre(Nlivre) ) ")
@@ -69,10 +71,115 @@ cur.execute(" INSERT INTO Resultat VALUES (9,3,13) ")
 
 cur.execute(" INSERT INTO Resultat VALUES (10,1,17) ")
 cur.execute(" INSERT INTO Resultat VALUES (10,2,18) ")
-cur.execute(" INSERT INTO Resultat VALUES (10,3,20) ")
+cur.execute(" INSERT INTO Resultat VALUES (10,3,20) ")"""
+
+def insBU(nomE):
+
+    cur.execute("SELECT Etudiant.dateInscriptionBU FROM Etudiant WHERE nomE=:nomE", {'nomE':nomE} )
+    rows = cur.fetchall()
+    for row in rows:
+        print(row)
+
+def insCour(num_cours):
+
+    cur.execute("SELECT nomE, prenomE FROM Etudiant AS E JOIN Resultat AS R ON(E.num_etu = R.num_etu) JOIN Cours AS C ON(R.num_cours = C.num_cours) WHERE C.num_cours=:num_cours", {'num_cours':num_cours} )
+    
+    rows = cur.fetchall()
+
+    for row in rows:
+        print(row) 
+
+def ResuEtu(num_etu):
+    cur.execute("SELECT nomE, prenomE,note,nomC,AVG(note) as moyenne,max(note) ,min(note) FROM Etudiant AS E JOIN Resultat AS R ON(E.num_etu = R.num_etu) JOIN Cours AS C ON(R.num_cours = C.num_cours) WHERE E.num_etu =:num_etu ", { 'num_etu':num_etu}  )
+    rows = cur.fetchall()
+
+    for row in rows:
+        print(row)
+
+def empLiv(Nlivre):
+
+    cur.execute("SELECT nomE, dateRetour FROM Etudiant AS E JOIN Pret AS P ON(E.num_etu=P.num_etu) JOIN Livre AS L ON(P.Nlivre=L.Nlivre) WHERE L.Nlivre=:Nlivre", {'Nlivre':Nlivre} )
+    
+    rows = cur.fetchall()
+
+    for row in rows:
+        print(row)
+
+def ResultTot():
+	cur.execute(" SELECT C.nomclass, Crs.nomC,AVG(note) AS Moyenne FROM Etudiant AS E JOIN Class AS C ON (E.numClasse=C.numClass) JOIN Resultat AS R ON(E.num_etu = R.num_etu) JOIN Cours AS Crs ON(R.num_cours = Crs.num_cours) GROUP BY C.nomclass, Crs.nomC ")
+	rows=cur.fetchall()
+	for row in rows:
+		print(row)
+
+def retard():
+
+    cur.execute("SELECT nomE FROM Etudiant AS E JOIN Pret AS P ON(E.num_etu=P.num_etu) JOIN Livre AS L ON(P.Nlivre=L.Nlivre) WHERE dateRetour> DateRetourPrevue" )
+    
+    rows = cur.fetchall()
+    for row in rows:
+    	print(row)
+
+def resultEchec():
+
+    cur.execute("SELECT note,avg(note) AS Moyenne, prenomE, nomE FROM Resultat AS R JOIN Etudiant AS E ON(R.num_etu = E.num_etu) JOIN Cours AS C ON(R.num_cours=C.num_cours) WHERE note<11 GROUP BY nomC" )
+    
+    rows = cur.fetchall()
+
+    for row in rows:
+        print(row)  
+def insc():
+    cur.execute(" SELECT nomE FROM Etudiant AS E JOIN Resultat AS R ON(E.num_etu=R.num_etu) JOIN Cours AS Crs ON(R.num_cours=Crs.num_cours) ")
+    rows=cur.fetchall()
+    for row in rows:
+        print(row)
+
+def changer_nomC(num_cours,nomC):
+   with conn:
+    cur.execute("UPDATE Cours SET nomC=:nomC WHERE num_cours=:num_cours", {'num_cours':num_cours, 'nomC':nomC})
+
+def supp_Cours(num_cours):
+    with conn:
+        cur.execute("DELETE FROM Cours WHERE num_cours=:num_cours",{'num_cours':num_cours}) 
+
+def noteSup14():
+    cur.execute("SELECT AVG(note) FROM Resultat GROUP BY num_etu HAVING AVG(note)>14")
+    return len(cur.fetchall())
+
+def note1214():
+    cur.execute("SELECT AVG(note) FROM Resultat GROUP BY num_etu HAVING AVG(note) BETWEEN 12 AND 14")
+    return len(cur.fetchall())
+
+def noteInf8():
+    cur.execute("SELECT AVG(note) FROM Resultat GROUP BY num_etu HAVING AVG(note)<8")
+    return len(cur.fetchall())
+
+def note810():
+    cur.execute("SELECT AVG(note) FROM Resultat GROUP BY num_etu HAVING AVG(note) BETWEEN 8 AND 10")
+    return len(cur.fetchall())
+
+def note1012():
+    cur.execute("SELECT AVG(note) FROM Resultat GROUP BY num_etu HAVING AVG(note) BETWEEN 10 AND 12")
+    return len(cur.fetchall())
+import matplotlib.pyplot as plt
+
+fig,ax = plt.subplots()
+ax.axis("equal")
+ax.pie([noteSup14(),note1214(),noteInf8(),note810(),note1012()],
+        shadow=True,
+        colors=["green","blue","orange","red","brown"],
+        labels = ["nb eleve note>14", "nb eleve 12<note<14","nb eleve note<8","nb elever 8<note<10","nb eleve 10<notew12"],
+        autopct="%1.1f"),
+plt.title("Graphe de Moyenne Generale")
+plt.show()  
+"""def nbEleves(note):
+    cur.execute("SELECT num_etu FROM Resultat GROUP BY num_etu HAVING AVG(note)=:note",{'note':note})
+    return len(cur.fetchall())
+
+x = [0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20]
+hauteurs_barres = [8,12,8,5,4,3,2,1,0,0]
+largeur_barres = 0.1
+plt.bar(x, hauteurs_barres, largeur_barres) 
+plt.show()"""
 
 
-
-
-conn.commit()
 conn.close()
